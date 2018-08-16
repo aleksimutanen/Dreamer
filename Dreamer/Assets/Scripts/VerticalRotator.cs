db44dba5855634ec1ca65de0a37d8cbdcdb714fb
Ari Punavuori <ari.punavuori@gmail.com>
2018-08-16 12:39:13 +0300

    Awesome Camera Controls v0.1

---
+++ Dreamer/Assets/Scripts/VerticalRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalRotator : MonoBehaviour {
+
+    public Transform target;
+    Quaternion maxRot;
+    public float vertSens = 5;
+    float xRot = 0;
+
+    void Update() {
+        xRot += Input.GetAxis("Mouse Y") * vertSens;
+        xRot = Mathf.Clamp(xRot, 10f, 45f);
+        //transform.localRotation = Quaternion.AngleAxis(Mathf.Clamp(xRot, 10, 45), Vector3.right) * transform.localRotation;
+        transform.localRotation = Quaternion.AngleAxis(xRot, Vector3.right);
+    }
+}
