0b1478bbf3be0eb9212746c458fc38ee8a997073
sara <sokeritahra@gmail.com>
2018-08-17 10:58:53 +0300

    oma scene, levelin loadaus alotettu, bat

---
+++ Dreamer/Assets/Bat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum BatMode {Hanging, Flying, Attacking};
+//tarviiko tän olla enum?
+
+public class Bat : MonoBehaviour {
+
+    //miten ja millon lepakot pitää luoda? aktivoida?
+    //pitääkö niiden olla jossain samassa folderissa? miks? miksei?
+
+    public BatMode batm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (batm == BatMode.Flying) {
+            Fly();
+        }
+	}
+
+    void Fly() {
+        //entiiä
+    }
+}
