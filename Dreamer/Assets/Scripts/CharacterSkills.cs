289bd5cacc67fbd75f4cb0a4b75cb185b60c267c
Aleksi Mutanen <mutanen.aleksi@hotmail.com>
2018-08-17 13:50:49 +0300

    started making skills for nightmaremode

--- Dreamer/Assets/Scripts/CharacterSkills.cs
+++ Dreamer/Assets/Scripts/CharacterSkills.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class CharacterSkills : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject floatPiece;
+    public GameObject shield;
+    CharacterMover cm;
+
+    void Start() {
+        cm = FindObjectOfType<CharacterMover>();
+        //Input.GetKeyDown(KeyCode.j)
+    }
+
+    // Update is called once per frame
+    void Update() {
+        LessGravity();
+        Shield();
+    }
+
+    bool Shield() {
+        if (Input.GetAxis("Shield") > 0.3) {
+            print("shield");
+            shield.SetActive(true);
+            return true;
+        } else {
+            shield.SetActive(false);
+            return false;
+        }
+    }
+
+    bool LessGravity() {
+        if (Input.GetAxis("LessGravity") > 0 && cm.onGround) {
+            cm.gravity = cm.normalGravity / 2;
+            floatPiece.SetActive(true);
+            return true;
+        } else {
+            cm.gravity = cm.normalGravity;
+            floatPiece.SetActive(false);
+            return false;
+        }
+    }
 }
+
+
