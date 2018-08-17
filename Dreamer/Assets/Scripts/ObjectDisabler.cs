bf892526a1319b857a5b216e26e5c29d18df58d1
Ari Punavuori <ari.punavuori@gmail.com>
2018-08-17 12:46:57 +0300

    ObjectEnabler, ObjectDisabler

---
+++ Dreamer/Assets/Scripts/ObjectDisabler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDisabler : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerExit(Collider other) {
+        other.gameObject.SetActive(false);
+    }
+}
