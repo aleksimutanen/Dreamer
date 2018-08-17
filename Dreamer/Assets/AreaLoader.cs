33229c08c4c102ae0c2850fc892aa73e7817bcf2
sara <sokeritahra@gmail.com>
2018-08-17 12:19:36 +0300

    area loader jee

---
+++ Dreamer/Assets/AreaLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaLoader : MonoBehaviour {
+
+    void SetChildrenActive(bool active) {
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(active);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == 10) {
+            print("moi");
+            SetChildrenActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.layer == 10) {
+            print("heihei");
+            SetChildrenActive(false);
+        }
+    }
+}
