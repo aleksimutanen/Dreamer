43f40858aa16935b350435ede06816b653194cee
sara <sokeritahra@gmail.com>
2018-08-20 16:08:42 +0300

    lepakko alotettu

--- Dreamer/Assets/Scripts/Bat.cs
+++ Dreamer/Assets/Scripts/Bat.cs
@@ -9,11 +9,16 @@ public class Bat : MonoBehaviour {
     //miten ja millon lepakot pitää luoda? aktivoida?
     //pitääkö niiden olla jossain samassa folderissa? miks? miksei?
 
+    Rigidbody rb;
+    public float speed;
     public BatMode batm;
+    RaycastHit hit2;
+    public float sphereRadius = 2;
+    public float maxDist = 2;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +29,32 @@ public class Bat : MonoBehaviour {
 	}
 
     void Fly() {
+        RaycastHit hit;
+
+        Physics.SphereCast(transform.position, sphereRadius, transform.forward, out hit);
+        hit2 = hit;
+
+        //print(hit.distance);
+        if (hit.collider != null && hit.distance < maxDist) {
+            print("väistää");
+            rb.velocity = new Vector3(0, 0, 0);
+            var targetPoint = transform.position + transform.right;
+            var targetRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+        }
+        else {
+            print("kääntyy");
+            rb.velocity = transform.forward * speed;
+            var targetPoint = transform.position + transform.right * 0.2f;
+            var targetRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 0.5f);
+        }
         //entiiä
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Debug.DrawLine(transform.position, transform.position + transform.forward * hit2.distance);
+        Gizmos.DrawWireSphere(transform.position + transform.forward * hit2.distance, sphereRadius);
     }
 }
