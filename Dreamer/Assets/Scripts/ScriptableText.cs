4ba37a1985fcc839dfc5c19cfde1e5e999745be3
Ari Punavuori <ari.punavuori@gmail.com>
2018-09-07 10:25:32 +0300

    SemiFinalScene 2.0 + ScriptableObject

---
+++ Dreamer/Assets/Scripts/ScriptableText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct TimedText {
+    public string text;
+    public float timeToDisplay;
+}
+
+[CreateAssetMenu(fileName = "ScriptableText", menuName = "ScriptableText", order = 1)]
+public class ScriptableText : ScriptableObject {
+    public List<TimedText> texts;
+}
