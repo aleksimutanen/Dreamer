8e9f1ed00bd8798df75ed1893f6c792532b71079
sara <sokeritahra@gmail.com>
2018-09-19 13:20:05 +0300

    bat, teleport yms

---
+++ Dreamer/Assets/Scripts/Teleport.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        print("hit " + other);
+        GameManager.instance.TeleportToCheckPoint(true);
+        }
+    }
+
