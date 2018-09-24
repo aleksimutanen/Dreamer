06650f94da015fa1ad8c12c48abb75f88a78150f
Ari Punavuori <ari.punavuori@gmail.com>
2018-09-24 11:05:55 +0300

    2. tason puzzleja + glideEnabler

---
+++ Dreamer/Assets/GlideEnabler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlideEnabler : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        GameManager.instance.glideEnabled = true;
+    }
+}
