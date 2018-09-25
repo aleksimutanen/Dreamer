106d847b42c44eb7cea4a1c49b71d78b60181b78
Ari Punavuori <ari.punavuori@gmail.com>
2018-09-25 10:21:17 +0300

    Cave 3 Firing enabled

---
+++ Dreamer/Assets/FiringEnabler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringEnabler : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        GameManager.instance.firingEnabled = true;
+    }
+}
