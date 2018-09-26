7d93b245707346d1e33bb0248f305f4c9fc14603
Ari Punavuori <ari.punavuori@gmail.com>
2018-09-26 12:57:28 +0300

    1. level semifinal semicomplete

--- Dreamer/Assets/Scripts/TutorialTrigger.cs
+++ Dreamer/Assets/Scripts/TutorialTrigger.cs
@@ -7,26 +7,28 @@ public class TutorialTrigger : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
 
         if(other.gameObject.tag == "Player") {
-            GameManager.instance.SetCheckpoint();
-
 
             if(gameObject.name == "TutorialJumpTrigger") {
                 GameManager.instance.ChangeStatusText(GameManager.instance.tutorialTexts[3], 5); 
                 gameObject.SetActive(false);
                 GameManager.instance.jumpEnabled = true;
+                GameManager.instance.SetCheckpoint();
             }
             if(gameObject.name == "TutorialCrystalTrigger") {
                 GameManager.instance.ChangeStatusText(GameManager.instance.tutorialTexts[4], 5);
                 gameObject.SetActive(false);
                 GameManager.instance.switchEnabled = true;
+                GameManager.instance.SetCheckpoint();
             }
             if(gameObject.name == "TutorialSwitchTrigger") {
                 GameManager.instance.ChangeStatusText(GameManager.instance.tutorialTexts[5], 5);
                 gameObject.SetActive(false);
+                GameManager.instance.SetCheckpoint();
             }
             if(gameObject.name == "TutorialCrystalTrigger2") {
                 GameManager.instance.ChangeStatusText(GameManager.instance.tutorialTexts[6], 5);
                 gameObject.SetActive(false);
+                GameManager.instance.SetCheckpoint();
             }
             if(gameObject.name == "BashEnabler") {
                 GameManager.instance.ChangeStatusText(GameManager.instance.tutorialTexts[7], 5);
@@ -40,11 +42,9 @@ public class TutorialTrigger : MonoBehaviour {
                 GameManager.instance.ChangeStatusText(GameManager.instance.tutorialTexts[9], 5);
                 GameManager.instance.reflectionEnabled = true;
             }
-
-
-
-
-
         }
     }
+
+
+
 }
